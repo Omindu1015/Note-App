58f1ee88b40faeeb396480d7afc658a752311d92
Omindu1015 <omindu.20230092@iit.ac.lk>
2025-06-14 11:07:50 +0530

    added middleware and rate limiter and completeing the controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,28 @@
-import express from 'express';
-import notesRoutes from './routes/notesRoutes.js'; 
-import { connect } from 'mongoose';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connect } from "mongoose";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-
 const app = express();
-const PORT= process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
-connectDB();
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(rateLimiter);
 
-app.use("/api/notes", notesRoutes);  
+// app.use((req, res, next) => {
+//     console.log(`${req.method} request for '${req.url}'`);
+//     next();
+// }
+// );
 
+app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
-    }
-);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+  });
+});
